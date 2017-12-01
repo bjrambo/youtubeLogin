14888eb76b298cef909f0246ce991e877b99d4c4
BJRambo <sosifam@070805.co.kr>
2017-12-01 13:59:53 +0900

    init commit

---
+++ main.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace YoutubeLogin
+{
+    partial class YoutubeLogin
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(976, 12);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(118, 43);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Youtube Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 61);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1082, 600);
+            this.webBrowser.TabIndex = 2;
+            // 
+            // YoutubeLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1106, 673);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "YoutubeLogin";
+            this.Text = "Youtube Login";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubeLogin
+{
+    public partial class YoutubeLogin : Form
+    {
+        public YoutubeLogin()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            webBrowser.Url = new System.Uri("http://www.swhite523.com/");
+        }
+    }
+}
